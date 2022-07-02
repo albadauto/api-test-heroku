da0ad034376058f61bdd76ed0c3c8fed1aa52995
albadev <joseadauto923@gmail.com>
2022-07-02 15:32:30 -0300

    resolved

--- app/Controllers/Http/RecoversController.ts
+++ app/Controllers/Http/RecoversController.ts
@@ -1,13 +1,28 @@
-//import Mail from '@ioc:Adonis/Addons/Mail';
+import Mail from '@ioc:Adonis/Addons/Mail';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User';
 
 export default class RecoversController {
 
-    public async recoverPass({  request }: HttpContextContract){
+    public async recoverPass({ response, request }: HttpContextContract){
         try{
-            const email = request.body();
-            console.log(email)
-            
+            const { email } = request.body();
+            const resultOfMail = await User.findBy("email", email);
+            if (resultOfMail){
+                const code = Math.random().toString(36).slice(-10);
+                this.SendMail(`Código de recuperação: ${code}`,
+                    "joseadauto923@hotmail.com",
+                    email,
+                    "Recuperação de senha IMOBI PROJECT"
+                )
+                return response.status(201).json({
+                    message:"Email com código de verificação enviado"
+                })
+            }else{
+                return response.status(400).json({
+                    message:"Não foi encontrado este email no nosso sistema."
+                })
+            }
         }catch(err){
             console.log(err);
         }
@@ -15,17 +30,17 @@ export default class RecoversController {
 
 
 
-    // private async SendMail(content:string, from: string, to: string, subject:string){
-    //     try{
-    //         await Mail.send((message) => {
-    //             message
-    //             .from(from)
-    //             .to(to)
-    //             .subject(subject)
-    //             .text(content)
-    //         })
-    //     }catch(err){
-    //         console.log(err);
-    //     }
-    // }
+    private async SendMail(content:string, from: string, to: string, subject:string){
+        try{
+            await Mail.send((message) => {
+                message
+                .from(from)
+                .to(to)
+                .subject(subject)
+                .text(content)
+            })
+        }catch(err){
+            console.log(err);
+        }
+    }
 }
